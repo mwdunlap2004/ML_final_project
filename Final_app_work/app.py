211fed9d02122b2325a1efb66384186a29f584fc
Lucas Rayder <wwg2tp@virginia.edu>
2025-12-11 14:41:56 -0500

    Fix 3

--- Final_app_work/app.py
+++ Final_app_work/app.py
@@ -1,4 +1,4 @@
-.import pandas as pd
+import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
